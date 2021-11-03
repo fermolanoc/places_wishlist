585fa648c1ead71e9e12556b29d09c93548679b4
Fernando Molano <fm87usa@gmail.com>
2021-11-03 12:29:02 -0400

    add json testing data, add 2 test to homepage and not visited places

--- travel_wishlist/tests.py
+++ travel_wishlist/tests.py
@@ -1,3 +1,27 @@
+from django.http import response
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Place
+
+
+class TestHomePage(TestCase):
+    def test_load_home_page_shows_empty_list_for_empty_database(self):
+        home_page_url = reverse('place_list')
+        response = self.client.get(home_page_url)
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+        self.assertContains(response, 'You have no places in your Wishlist')
+
+
+class TestWishList(TestCase):
+
+    fixtures = ['test_places']
+
+    def test_viewing_wishlist_contains_not_visited_places(self):
+        response = self.client.get(reverse('place_list'))
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+
+        self.assertContains(response, 'Tokyo')
+        self.assertContains(response, 'New York')
+        self.assertNotContains(response, 'San Francisco')
+        self.assertNotContains(response, 'Moab')
