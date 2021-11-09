3e417292ee43a62464fdec3de2cf85c4bcdc898f
Fernando Molano <fm87usa@gmail.com>
2021-11-08 22:41:20 -0500

    add new form review to places that have been visited to include notes, photos, and dates, include review form on place_details HTML

--- travel_wishlist/views.py
+++ travel_wishlist/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
-from .forms import NewPlaceForm
+from .forms import NewPlaceForm, TripReviewForm
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
+from django.contrib import messages
 
 
 @login_required
@@ -56,7 +57,30 @@ def place_was_visited(request):
 @login_required
 def place_details(request, place_pk):
     place = get_object_or_404(Place, pk=place_pk)
-    return render(request, 'travel_wishlist/place_details.html', {'place': place})
+
+    # Does this place belong to the user?
+    if place.user != request.user:
+        return HttpResponseForbidden()
+
+    if request.method == 'POST':
+        form = TripReviewForm(request.POST, request.FILES, instance=place)
+        # instance is the moderl object to update with the form data
+
+        if form.is_valid():
+            form.save()
+            messages.info(request, 'Trip information updated!')
+        else:
+            # temp error version, needs improvement
+            messages.error(request, form.errors)
+        return redirect('place_details', place_pk=place_pk)
+
+    else:  # Get place details
+        if place.visited:
+            # pre-populate with data from this Place instance
+            review_form = TripReviewForm(instance=place)
+            return render(request, 'travel_wishlist/place_details.html', {'place': place, 'review_form': review_form})
+        else:
+            return render(request, 'travel_wishlist/place_details.html', {'place': place})
 
 
 @login_required
